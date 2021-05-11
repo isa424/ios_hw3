029160316b834dafadd87887ccb8c3a8c46f29a4
Isa Ishangulyyev <isaishan424@gmail.com>
2021-05-11 16:25:09 +0300

    Final Commit

---
+++ IsaIshangulyyev_HW3/XmlVC.swift
@@ -0,0 +1,29 @@
+//
+//  XmlVC.swift
+//  IsaIshangulyyev_HW3
+//
+//  Created by Isa Ishangulyyev on 11.05.2021.
+//
+
+import UIKit
+
+class XmlVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
